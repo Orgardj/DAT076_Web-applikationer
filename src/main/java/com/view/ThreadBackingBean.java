abf56a4f50790a96443bf45c7d63a96506bc11f5
uhrej <uhrej@student.chalmers.se>
2020-03-09 17:47:43 +0100

    create thread beta

--- src/main/java/com/view/ThreadBackingBean.java
+++ src/main/java/com/view/ThreadBackingBean.java
@@ -10,25 +10,56 @@ package com.view;
  * @author Team J
  */
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Data;
+import model.dao.AccountDAO;
+import model.dao.CategoryDAO;
+import model.dao.PostDAO;
 import model.dao.ThreadDAO;
+import model.entity.Post;
 import model.entity.Thread;
+import org.omnifaces.cdi.Param;
 
 @Data
 @Named
 @ViewScoped
 public class ThreadBackingBean implements Serializable {
-
+    
+    private String enteredTitle;
+    
+    private String enteredMessage;
+    
     @EJB
     private ThreadDAO threadDAO;
+    
+    @EJB
+    private CategoryDAO categoryDAO;
+    
+    @EJB
+    private PostDAO postDAO;
+    
+    @EJB
+    private AccountDAO accountDAO;
+    
+    @Inject
+    @Param
+    private long id;
 
     private List<Thread> threads;
-
+    
     public List<Thread> getMatchingThreads(Long tId) {
         return threadDAO.findThreadsMatchingCategory(tId);
+    }
+    
+    public void createThread() {
+        Thread thread = new Thread(enteredTitle, Long.valueOf(5), new Date(), categoryDAO.find(id), new ArrayList<>());
+        threadDAO.create(thread);
+        postDAO.create(new Post(enteredMessage, new Date(), accountDAO.findAccountMatchingUserName("john23"), thread));
     }
 }

46b448efce0a93ecf38209dc538e6f9cc0d8b959
Jonathan Edfeldt <jonathan.97@live.se>
2020-03-18 21:42:01 +0100

    Gui bugs fix

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -7,61 +7,77 @@ $(document).ready(function () {
 
     $("a").on('click', function (event) {
         var hash = this.hash;
-        alert($(hash).offset().top - 70);
+        
+        var scrollAmount = $(hash).offset().top - 70;
 
-        /*
-         if (this.hash !== "") {
-         
-         event.preventDefault();
-         // Store hash
-         var hash = this.hash;
-         $('html, body').animate({
-         scrollTop: $(hash).offset().top - 30    
-         }, 1100, function(){
-         window.location.hash = hash;
-         });
-         } 
-         * 
-         */  
+        if (this.hash !== "") {
+
+            event.preventDefault();
+            
+            
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: scrollAmount
+            }, 700, function () {
+                //window.location.hash = hash;
+            });
+        }
+
     });
-    
+
 });
 
 
-function displayForm(input){
+function displayForm(input) {
     //alert(input);
-    
-    if(input === "open"){
+
+    if (input === "open") {
         $(".blackCover").css("display", "block");
-    }
-    else if(input === "close"){
+    } else if (input === "close") {
         $(".blackCover").css("display", "none");
     }
 }
 
 
 
-function inputValue(input){
+function inputValue(input) {
     //alert(input);
 
-    if($("." + input + " input").val()){
+    if ($("." + input + " input").val()) {
         $("." + input + " label").addClass("keepOverHeadPlaceholder");
-    }
-    else if(!$("." + input + " input").val()){
+    } else if (!$("." + input + " input").val()) {
         $("." + input + " label").removeClass("keepOverHeadPlaceholder");
     }
-    
+
     //alert($("." + input + " input").val());
 }
 
 
-function switchForm(type){
-    if(type === "log"){
+function switchForm(type) {
+    if (type === "log") {
         $(".loginForm").css("display", "none");
         $(".regForm").css("display", "block");
-    }
-    else if(type === "reg"){
+    } else if (type === "reg") {
         $(".regForm").css("display", "none");
         $(".loginForm").css("display", "block");
+    }
+}
+
+
+if ($(".regForm span").length) {
+    //alert($(".regForm span").length);
+
+    var i;
+
+    for (i = 0; i < $(".regForm span").length; i++) {
+        //console.log($(".regForm span").parent().get( 0 ).tagName);
+        $(".regForm span").parent().children("label").css("top", "calc(50% - 1em)");
+    }
+}
+
+for (i = 0; i <= $(".regForm div input").length; i++) {
+    //console.log($(".regForm div:nth-child(" + i + ") input").val());
+    if ($(".regForm div:nth-child(" + i + ") input").val() !== "j_idt13:studentForm" && $(".regForm div:nth-child(" + i + ") input").val() !== "") {
+        $(".regForm div:nth-child(" + i + ") label").addClass("keepOverHeadPlaceholder");
     }
 }
